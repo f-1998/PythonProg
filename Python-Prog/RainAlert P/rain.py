434ef111f6c9eb547e2b4f1a3d6622024a2ea93e
Faiza Ghayas <60214718+f-1998@users.noreply.github.com>
2023-10-31 16:19:53 +0530

    rain.py

--- Python-Prog/RainAlert P/rain.py
+++ Python-Prog/RainAlert P/rain.py
@@ -1,10 +1,10 @@
 import requests
 from twilio.rest import Client
 
-api_key ="69f04e4613056b159c2761a9d9e664d2"  #your api key from weather data
+api_key ="69f04e4613056b1"  #your api key from weather data
 
-account_sid = 'AC3234248f979bcf7b5274fd8516d84e59' #twilio sid
-auth_token = '5cc4cd3ff488b32ee05a5d1612b0bf04' #twilio auth_token
+account_sid = '2345ghfhgdtrdr' #twilio sid
+auth_token = '52345ghfhgdtrdr' #twilio auth_token
 
 response = requests.get(url=f"https://api.openweathermap.org/data/2.5/onecall?lat=22.572645&lon=88.363892&exclude=daily,current,minutely,alerts&appid={api_key}")
 
@@ -24,8 +24,8 @@ if not not_rain:
     message = client.messages \
         .create(
         body="Bring an Umbrella.☔",
-        from_='+12512377975', #Twilio phone number
-        to='+917394992184'  # receiver phone number verified
+        from_='+1251239067', #Twilio phone number
+        to='+01123456789'  # receiver phone number verified
     )
 
 
